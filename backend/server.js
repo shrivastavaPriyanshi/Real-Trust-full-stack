229679510e041bdb3570384fb2d474cfd00344d4
shrivastavaPriyanshi <priyanshi24july@gmail.com>
2025-12-19 21:56:39 +0530

    add mongo uri

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,25 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
+
 
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected ✅");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error ❌", err);
+  });
 
 const projectRoutes = require("./routes/projectRoutes");
 const clientRoutes = require("./routes/clientRoutes");
@@ -20,21 +32,8 @@ app.use("/api/contact", contactRoutes);
 app.use("/api/subscribe", subscriberRoutes);
 
 
-// Test route
-app.get("/", (req, res) => {
-  res.send("Backend is running 🚀");
-});
 
 const PORT = process.env.PORT || 5000;
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB Connected ✅");
-  })
-  .catch((err) => {
-    console.error("MongoDB connection error ❌", err);
-  });
 
 
 app.listen(PORT, () => {
